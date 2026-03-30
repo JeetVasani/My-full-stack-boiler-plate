739944b85438c78cea1b2bdd7cce104bc5fe8642
Jeet Vasani <21jeetvasani@gmail.com>
2026-03-30 12:02:26 +0530

    Delete Desktop/Automations/frontend/my-app directory

--- Desktop/Automations/frontend/my-app/app/(pages)/user_profile/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useEffect, useState } from "react";
-import { getProfile } from "@/src/lib/api";
-
-export default function ProfilePage() {
-  <h1>User Profile</h1>;
-}
--- Desktop/Automations/frontend/my-app/app/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-gray-900 text-gray-400 border-t border-gray-800 py-4 mt-10">
-      <div className="w-full text-center text-sm">
-        © 2025 Automation Website. All rights reserved.
-      </div>
-    </footer>
-  );
-}
